8065f9c08fc182ceb654d110e0d70e53d072f3a5
Ultisaer <ultimum.sanae@gmail.com>
2023-07-01 18:18:49 -0500

    Creacion de la barra desplegable del carrito de compras / trancisiones CSS de los elementos interactivos / codigo escalable JS

--- main.js
+++ main.js
@@ -1,33 +1,59 @@
-const menuEmail   = document.querySelector(".navbar-email");
+// Selección de elementos del DOM
+const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const burguerMenu = document.querySelector(".menu");
-const mobileMenu  = document.querySelector(".mobile-menu");
-const body        = document.querySelector("body");
+const menuCarIcon = document.querySelector(".navbar-shopping-cart");
+const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
-menuEmail.addEventListener("click", function(event){
-    event.stopPropagation(); // Evita la propagación del evento
-    toggleDesktopMenu();
-});
-burguerMenu.addEventListener("click", function(event) {
-    event.stopPropagation(); // Evita la propagación del evento
-    toggleMobileMenu();
-});
+// Agrupación de elementos que podrían tener la clase 'active'
+const allElementsHTML   = [menuEmail, menuCarIcon, burguerMenu]
+const allActiveElements = [desktopMenu, aside, mobileMenu];
 
-body.addEventListener("click", function() {
-    if(mobileMenu.classList.contains("active") || desktopMenu.classList.contains("active")) {
-        toggleDesktopMenu();
-        toggleMobileMenu();
-    }  
-});
-
-function toggleDesktopMenu() {
-    menuEmail.classList.toggle("animation-nav");
-    desktopMenu.classList.toggle("inactive");
-    desktopMenu.classList.toggle("active");
+// Función para eliminar el estado activo de todos los elementos
+function removeActiveState() {
+    allActiveElements.forEach(element => {
+        element.classList.remove("active");
+        element.classList.add("inactive");
+    });
+    allElementsHTML.forEach(element => {
+        element.classList.remove("animation-nav");
+    });
 }
 
-function toggleMobileMenu() {
-    burguerMenu.classList.toggle("animation-svg");
-    mobileMenu.classList.toggle("inactive");
-    mobileMenu.classList.toggle("active");
+// Función para alternar el estado activo del elemento seleccionado
+function toggleActiveState(element) {
+    if (element.classList.contains("inactive")) {
+        removeActiveState();
+    }
+    element.classList.toggle("active");
+    element.classList.toggle("inactive");
+    
+    let index = allActiveElements.indexOf(element);    
+    if (index !== -1) {
+        allElementsHTML[index].classList.toggle("animation-nav");
+    }
 }
+
+// Función para agregar un escuchador de eventos de clic a un elemento
+function addToggleListener(element, active) {
+    element.addEventListener("click", function(event){
+        event.stopPropagation();
+        toggleActiveState(active);
+    });
+} 
+
+// Agregación de escuchadores de eventos a los elementos
+for(let i = 0; i < allActiveElements.length ; i++){
+    addToggleListener(allElementsHTML[i], allActiveElements[i]);
+}
+
+// Agregación de escuchador de eventos al documento
+document.addEventListener("click", function(event) {
+    for (let i = 0; i < allActiveElements.length; i++) {
+        if (allActiveElements[i].contains(event.target)) {
+            return;
+        }
+    }
+    removeActiveState();
+});
